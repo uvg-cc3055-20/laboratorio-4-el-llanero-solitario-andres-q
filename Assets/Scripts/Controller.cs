ad08e87936433dfd1cee81d4563f43bc049bfed9
LoboAnimae <30876166+LoboAnimae@users.noreply.github.com>
2018-02-12 17:24:44 -0600

    Added everything lol

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Controller : MonoBehaviour {
+	public static Controller instance;
+	public float ellapsedTime;
+	public Text seconds;
+	public bool status;
+
+
+	// Use this for initialization
+	void Start () {
+		instance = this;
+		ellapsedTime = 0;
+		status = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Assumes player is alive
+		if(status = true){
+			ellapsedTime = ellapsedTime + Time.deltaTime;
+	}
+		//Assumes player is dead
+		if(status = false){
+			PlayerPrefs.SetFloat ("Highscore", ellapsedTime);
+			Load (0);
+		}
+	}
+
+	public void Load(int val){
+		SceneManager.LoadScene (val);
+	}
+}
---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour {
+	private Rigidbody2D rb;
+	public GameObject laser;
+	public float velocity;
+	public float left;
+	public float right;
+	public Transform laser0;
+	public Transform laser1;
+
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		velocity = 4f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		#if UNITY_ANDROID
+		float movX = Input.acceleration.x;
+		rb.transform.Translate(Vector2.right * velocity * movX *
+			Time.deltaTime);
+		#endif
+
+	}
+
+
+	private void OnCollisionEnter2D(Collision2D collision){
+		Controller.instance.status = false;
+}
+}
